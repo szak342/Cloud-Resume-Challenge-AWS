ed1ee0eb516eb9f5c90a44fd29c046d5df830019
szak342 <krzysztof.szadkowski@gmail.com>
2023-11-11 18:55:30 +0100

    Working on lambda code

--- SAM/Resume/resume_read/app.py
+++ SAM/Resume/resume_read/app.py
@@ -5,34 +5,41 @@ client = boto3.client('dynamodb')
 table='resume-ResumeTable-1Q1PSLSXHEWAT'
 COUNTER = False
 
-def return_data():
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-        })}
-
-def create_new_counter():
-    item = {'id':{'S':"Counter"}, count:{"N", "1"}}
-    data = client.put_item(TableName=table,Item=data)
-    pass
-
-def add_visitor(x):
-    x = int(x) + 1
-    item = {'id':{'S':"Counter"}, count:{"N", str(int(x) + 1)}}
-    data = client.put_item(TableName=table,Item=data)
-
-def check_if_counter_exist():
-    if COUNTER == False:
-        try:
-            item = {'id':{'S':"Counter"}}
-            data = client.get_item(TableName=table,Key=item)
-            COUNTER = True
-            #print(data["Item"]["count"]["N"])
-        except:
-            data = client.put_item(TableName=table,Item=data)
+class DataObject():
+    def __init__(self) -> None:
+        self.COUNTER = False
+        pass
         
+    def return_data(self):
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "hello world2"
+            })}
+
+    def create_new_counter(self):
+        item = {'id':{'S':"Counter"}, "count":{"N", "1"}}
+        data = client.put_item(TableName=table,Item=item)
+
+    def add_visitor(self, x):
+        x = int(x) + 1
+        item = {'id':{'S':"Counter"}, "count":{"N", str(int(x) + 1)}}
+        data = client.put_item(TableName=table,Item=item)
+
+    def get_item(self):
+        item = {'id':{'S':"Counter"}}
+        data = client.get_item(TableName=table,Key=item)
+        return data["Item"]["count"]["N"]
+
+    def check_if_counter_exist(self):
+        try:
+            x = self.get_item()
+            COUNTER = True
+        except:
+            self.create_new_counter(self)
+
+dbconnector = DataObject()    
 
 def lambda_handler(event, context):
-    check_if_counter_exist()
-    return return_data
+    dbconnector.check_if_counter_exist()
+    return dbconnector.return_data()
