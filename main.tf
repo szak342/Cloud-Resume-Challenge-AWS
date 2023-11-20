12969cbd465df0e3300b925d4dc35637eaf6e3df
Chris <Chris@example2314.com>
2023-11-20 16:08:03 +0100

    Added config.yml and variable to website.yml for automatic upload to bucket

--- main.tf
+++ main.tf
@@ -398,3 +398,14 @@ output "cloudfront_address" {
 output "api_address" {
   value = aws_api_gateway_deployment.deployment.invoke_url
 }
+
+
+resource "local_file" "config" {
+    content = templatefile("templates/config.tpl",{
+      invoke_url = aws_api_gateway_deployment.deployment.invoke_url
+      s3bucket = aws_s3_bucket.resume-bucket.id
+      cloudfront_url = aws_cloudfront_distribution.resume_cf_distribution.domain_name
+      })
+    filename = ".github/workflows/config.yml"
+    #depends_on = [ aws_api_gateway_deployment.deployment ]
+}
---
+++ templates/config.tpl
@@ -0,0 +1,4 @@
+
+cloudfront_url: ${cloudfront_url}
+s3bucket: ${s3bucket}
+api_invoke_url: ${invoke_url}
