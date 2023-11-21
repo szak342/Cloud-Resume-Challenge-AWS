d0a94aeb2eb19b05f8d1dba0c0512c1fc0fbc83c
Chris <Chris@example2314.com>
2023-11-21 16:24:56 +0100

    testing website sync build action with variables

--- webpage/script.js
+++
@@ -1,8 +0,0 @@
-function getData() {
-    fetch("https://2k3dqst9w1.execute-api.eu-west-1.amazonaws.com/prod/dev")
-    .then(res => res.json())
-    .then(data => document.getElementById("show").innerHTML = data.body.message)
-    .catch(error => console.log("Error: ", error))
-};
-
-getData()
