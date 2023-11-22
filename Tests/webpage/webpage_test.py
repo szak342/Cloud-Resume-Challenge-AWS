b83f69b538d973673963c0ca8eb12ef50e9e0533
Chris <Chris@example2314.com>
2023-11-22 09:24:56 +0100

    Created Test folder

---
+++ Tests/webpage/webpage_test.py
@@ -0,0 +1,17 @@
+import json
+import os
+import pytest
+
+def test_api_link(js):
+    assert True
+
+def test_name_of_files(files):
+    assert files == ['index.html', 'script.js', 'styles.css']
+
+def test_webpage(folder):
+    files = os.listdir(folder)
+
+
+
+    
+
