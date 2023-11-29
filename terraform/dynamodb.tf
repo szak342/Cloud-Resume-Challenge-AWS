6c627cd871e87c18b3df8fe51178d164426e8ed5
Chris <Chris@example2314.com>
2023-11-29 15:12:19 +0100

    Work on null-resource local-exec to enable backup and restoration of the dynamodb database

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,20 +1,17 @@
 resource "aws_dynamodb_table" "resume-dynamodb" {
-  name             = "resume-table"
-  billing_mode     = "PAY_PER_REQUEST"
-  hash_key       = "id"
+  name         = "resume-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
     name = "id"
     type = "S"
   }
-  server_side_encryption { enabled = true } 
+  server_side_encryption { enabled = true }
   tags = {
     Name        = "dynamodb-resume-table"
     Environment = "dev"
   }
-#  lifecycle {
-#    prevent_destroy = true
-#  }
 }
 
 resource "aws_dynamodb_table_item" "example" {
@@ -28,3 +25,34 @@ resource "aws_dynamodb_table_item" "example" {
 }
 ITEM
 }
+
+resource "null_resource" "create_dynamodb_backup" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = "aws dynamodb create-backup --table-name ${aws_dynamodb_table.resume-dynamodb.name} --backup-name backup-${aws_dynamodb_table.resume-dynamodb.name}"
+  }
+}
+
+
+resource "null_resource" "restore_dynamodb_backup" { #with deletion of current table
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    environment = {
+      env = ".env"
+    }
+    command     = <<-EOT
+    latest_backup_arn=$(aws dynamodb list-backups --table-name ${aws_dynamodb_table.resume-dynamodb.name} --query "BackupSummaries | [-1].BackupArn" --output text)
+    aws dynamodb delete-table --table-name ${aws_dynamodb_table.resume-dynamodb.name}
+    aws dynamodb restore-table-from-backup --target-table-name ${aws_dynamodb_table.resume-dynamodb.name} --backup-arn $latest_backup_arn
+    EOT
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = "aws dynamodb restore-table-from-backup --target-table-name ${aws_dynamodb_table.resume-dynamodb.name} --backup-arn $latest_backup_arn"
+  }
+}
+
+
+
