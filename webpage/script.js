777448ce8aa97faa89a28f890590d66d0be7e341
Chris <Chris@example2314.com>
2023-11-29 15:13:52 +0100

    Working on html and css

--- webpage/script.js
+++ webpage/script.js
@@ -1,5 +1,5 @@
 function getData() {
-    fetch("https://1z88tmga8j.execute-api.eu-west-1.amazonaws.com/prod/dev")
+    fetch("https://72q0ob9uyl.execute-api.eu-west-1.amazonaws.com/prod/dev")
     .then(res => res.json())
     .then(data => document.getElementById("show").innerHTML = data.body.message)
     .catch(error => console.log("Error: ", error))
