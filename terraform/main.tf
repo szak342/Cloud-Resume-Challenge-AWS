aff95f25b9c0ac3af43beaa8bf5c462074a86ad9
Chris <Chris@example2314.com>
2023-11-29 10:47:05 +0100

    Added commands for removing and uploading index.html

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,6 +26,19 @@ locals {
     account_id = data.aws_caller_identity.current.account_id
 }
 
+resource "null_resource" "shutdown_website" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command = "aws s3 rm s3://${aws_s3_bucket.resume-bucket.id}/index.html"
+  }
+}
+
+resource "null_resource" "start_website" {
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command = "aws s3 cp ../webpage/index.html s3://${aws_s3_bucket.resume-bucket.id}/"
+  }
+}
 
 
 
