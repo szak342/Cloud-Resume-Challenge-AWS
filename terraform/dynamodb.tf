dd434deb50d1932f099b5eedda8ece73f44c0d8f
Chris <Chris@example2314.com>
2023-11-30 16:13:34 +0100

    comments added

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -25,8 +25,10 @@ resource "aws_dynamodb_table_item" "example" {
 }
 ITEM
 }
+# TODO ----------
 
 resource "null_resource" "create_dynamodb_backup" {
+  count = var.enable_create_dynamodb_backup ? 1 : 0
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     command     = "aws dynamodb create-backup --table-name ${aws_dynamodb_table.resume-dynamodb.name} --backup-name backup-${aws_dynamodb_table.resume-dynamodb.name}"
@@ -35,24 +37,18 @@ resource "null_resource" "create_dynamodb_backup" {
 
 
 resource "null_resource" "restore_dynamodb_backup" { #with deletion of current table
-
+  count = var.enable_restore_dynamodb_backup ? 1 : 0
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
     environment = {
       env = ".env"
     }
-    command     = <<-EOT
+    command = <<-EOT
     latest_backup_arn=$(aws dynamodb list-backups --table-name ${aws_dynamodb_table.resume-dynamodb.name} --query "BackupSummaries | [-1].BackupArn" --output text)
     aws dynamodb delete-table --table-name ${aws_dynamodb_table.resume-dynamodb.name}
     aws dynamodb restore-table-from-backup --target-table-name ${aws_dynamodb_table.resume-dynamodb.name} --backup-arn $latest_backup_arn
     EOT
   }
-
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = "aws dynamodb restore-table-from-backup --target-table-name ${aws_dynamodb_table.resume-dynamodb.name} --backup-arn $latest_backup_arn"
-  }
 }
-
 
 
