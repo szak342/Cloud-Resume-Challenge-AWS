44acdb5e5302f618c7afabce51687ca138ccd951
Chris <Chris@example2314.com>
2023-12-01 16:21:22 +0100

    Working on html and css

--- webpage/script.js
+++
@@ -1,8 +0,0 @@
-function getData() {
-    fetch("https://xx799tk2r9.execute-api.eu-west-1.amazonaws.com/prod/dev")
-    .then(res => res.json())
-    .then(data => document.getElementById("show").innerHTML = data.body.message)
-    .catch(error => console.log("Error: ", error))
-};
-
-getData()
