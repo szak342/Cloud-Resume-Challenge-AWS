6b5aa165a760330858379663c5a5bf965b14dd16
Chris <Chris@example2314.com>
2023-12-11 14:42:09 +0100

    testing

--- webpage/script.js
+++ webpage/script.js
@@ -1,5 +1,5 @@
 function getData() {
-    fetch("https://va3q42ycuk.execute-api.eu-west-1.amazonaws.com/prod/dev")
+    fetch("https://0xvw2n4qi4.execute-api.eu-west-1.amazonaws.com/prod/dev")
     .then(res => res.json())
     .then(data => document.getElementById("show").innerHTML = data.body.message)
     .catch(error => console.log("Error: ", error))
