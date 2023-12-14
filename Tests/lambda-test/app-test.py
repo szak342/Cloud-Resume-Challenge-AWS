e85332227f1e52da233fdfce0d41c711f60ce460
szak342 <krzysztof.szadkowski@gmail.com>
2023-12-14 18:19:11 +0100

    modifying the test for the new lambda code

--- Tests/lambda-test/app-test.py
+++ Tests/lambda-test/app-test.py
@@ -4,18 +4,27 @@ from pytest import MonkeyPatch
 import moto
 import os
 import sys
+import json
 
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../') + 'sam-app/app/')
 sys.path.append(path)
 
+origin_header = "www.krzysztofszadkowski.com"
+
 MonkeyPatch().setenv("DDB_TABLE", "test_table")
 MonkeyPatch().setenv("AWS_ACCESS_KEY_ID", "testing")
 MonkeyPatch().setenv("AWS_SECRET_ACCESS_KEY", "testing")
+MonkeyPatch().setenv("ALLOWED_SITES","https://krzysztofszadkowski.com,https://www.krzysztofszadkowski.com")
 
 from app import *
 
-RETURN_DATA = {"statusCode": 200, "body":{ "message": 1}}
+RETURN_DATA = {"statusCode": 200, 
+               "body":json.dumps({ "message": 1}), 
+               "headers": {
+                    "Access-Control-Allow-Origin" : origin_header
+                },
+                "isBase64Encoded": False}
 
 @moto.mock_dynamodb
 def test_lambda_handler():
@@ -32,4 +41,4 @@ def test_lambda_handler():
     item = {"id": "Counter","count": 0}
     table.put_item(Item=item)
     
-    assert return_data(add_visitor(get_item())) == RETURN_DATA
+    assert return_data(add_visitor(get_item()), origin_header) == RETURN_DATA
