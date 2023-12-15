0791166e12b7b08581bad3cc5756706194a68f89
szak342 <krzysztof.szadkowski@gmail.com>
2023-12-15 14:59:16 +0100

    script.js

--- webpage/script.js
+++ webpage/script.js
@@ -1,5 +1,5 @@
 function getData() {
-    fetch("https://teb4908ej4.execute-api.eu-west-1.amazonaws.com/prod/dev")
+    fetch("https://p98rk158vj.execute-api.eu-west-1.amazonaws.com/prod/dev")
     .then(res => res.json())
     .then(data => document.getElementById("show").innerHTML = data.message)
     .catch(error => console.log("Error: ", error))
