98f5442127cc07a15c32c6a03e621f39690db678
Chris <Chris@example2314.com>
2023-12-27 15:37:43 +0100

    cleanup

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,6 @@
 resource "aws_lambda_function" "resume-lambda" {
   function_name = "resume-lambda"
 
-  # The bucket name as created earlier with "aws s3api create-bucket"
-  #s3_bucket = "main-bucket-28357"
-  #s3_key    = "lambda/ResumeReadFunction.zip"
-
   filename = "resumelambda.zip"
   handler = "app.lambda_handler"
   runtime = "python3.11"
@@ -80,8 +76,5 @@ resource "aws_lambda_permission" "apigw" {
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.resume-lambda.function_name}"
   principal     = "apigateway.amazonaws.com"
-
-  # The /*/* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
   source_arn = "${aws_api_gateway_rest_api.resume-api.execution_arn}/*/*" # Sus
 }
