a546dac2e4e7a249c6bcacad90d0ee6209798531
Chris <Chris@example2314.com>
2023-12-27 15:35:25 +0100

    deleted unused code

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -25,30 +25,3 @@ resource "aws_dynamodb_table_item" "example" {
 }
 ITEM
 }
-# TODO ----------
-
-resource "null_resource" "create_dynamodb_backup" {
-  count = var.enable_create_dynamodb_backup ? 1 : 0
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    command     = "aws dynamodb create-backup --table-name ${aws_dynamodb_table.resume-dynamodb.name} --backup-name backup-${aws_dynamodb_table.resume-dynamodb.name}"
-  }
-}
-
-
-resource "null_resource" "restore_dynamodb_backup" { #with deletion of current table
-  count = var.enable_restore_dynamodb_backup ? 1 : 0
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-c"]
-    environment = {
-      env = ".env"
-    }
-    command = <<-EOT
-    latest_backup_arn=$(aws dynamodb list-backups --table-name ${aws_dynamodb_table.resume-dynamodb.name} --query "BackupSummaries | [-1].BackupArn" --output text)
-    aws dynamodb delete-table --table-name ${aws_dynamodb_table.resume-dynamodb.name}
-    aws dynamodb restore-table-from-backup --target-table-name ${aws_dynamodb_table.resume-dynamodb.name} --backup-arn $latest_backup_arn
-    EOT
-  }
-}
-
-
