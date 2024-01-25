2c12190d480d7152a83533942d9b90c1f030ea0f
Chris <Chris@example2314.com>
2024-01-25 13:42:40 +0100

    edited for new website

--- Tests/webpage/webpage_test.py
+++ Tests/webpage/webpage_test.py
@@ -6,19 +6,18 @@ import sys
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../') + 'webpage/')
 sys.path.append(path)
 
-files = ['img', 'index.html', 'script.js', 'styles.css']
-images = ["achievement.png", "AWSCCP.png", "AWSCDA.png", "AWSCSA.png", "email.png", "github.png", "gps.png", "linkedin.png"]
+files = ['bootstrap', 'devicons', 'font-awesome', 'jquery', 'jquery-easing', 'simple-line-icons', 'gulpfile.js', 'index.html', 'LICENSE', 'package.json', 'README.md']
+
+files = sorted(files)
 
 def test_api_link():
     assert True
 
 def test_root_folder():
     output = os.listdir(path)
+    output = sorted(output)
     assert output == files
 
-def test_img_folder():
-    output = os.listdir(f"{path}/img")
-    assert output == images
 
 
 
