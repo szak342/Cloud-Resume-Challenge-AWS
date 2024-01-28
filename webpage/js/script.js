ce0b6acb71ddb896d361ea9ad0967265f98f2a63
Krzysztof Szadkowski <68223367+szak342@users.noreply.github.com>
2024-01-28 18:23:28 +0100

    script.js from terraform

--- webpage/js/script.js
+++
@@ -1,8 +0,0 @@
-function getData() {
-    fetch("https://5yyuqcudj8.execute-api.eu-west-1.amazonaws.com/prod/dev")
-    .then(res => res.json())
-    .then(data => document.getElementById("show").innerHTML = data.message)
-    .catch(error => console.log("Error: ", error))
-};
-
-getData()
