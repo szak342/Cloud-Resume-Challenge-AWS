b3084d7578cca984dea1ef2da28e11e1aa373ba7
Chris <Chris@example2314.com>
2024-02-01 11:39:00 +0100

    removed unused config template

--- terraform/templates.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "config" {
-    content = templatefile("templates/config.tpl",{
-      invoke_url = aws_api_gateway_deployment.deployment.invoke_url
-      s3bucket = aws_s3_bucket.resume-bucket.id
-      cloudfront_url = aws_cloudfront_distribution.resume_cf_distribution.domain_name
-      cloudfront_id = aws_cloudfront_distribution.resume_cf_distribution.id
-      })
-    filename = "config"
-}
