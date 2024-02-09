8a4d2b23f28159517f3094c2a294949b9802f6cd
Chris <Chris@example2314.com>
2024-02-09 09:13:37 +0100

    added role and policies for dynamodb streams

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -38,20 +38,86 @@ data "aws_iam_policy_document" "allow_access_from_cloud_front" {
     principals {
       type        = "Service"
       identifiers = ["cloudfront.amazonaws.com"]
-    } 
+    }
 
     effect = "Allow"
-    
+
     actions = ["s3:GetObject"]
-    
+
     resources = [
       "${aws_s3_bucket.resume-bucket.arn}/*",
     ]
 
     condition {
-      test = "StringEquals"
+      test     = "StringEquals"
       variable = "AWS:SourceArn"
-      values = [aws_cloudfront_distribution.resume_cf_distribution.arn]
+      values   = [aws_cloudfront_distribution.resume_cf_distribution.arn]
     }
   }
+}
+
+resource "aws_iam_role" "lambda_assume_role" {
+  name               = "lambda-dynamodb-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": "LambdaAssumeRole"
+    }
+  ]
+}
+EOF
+
+  inline_policy {
+    name   = "lambda-to-sns-policy"
+    policy = data.aws_iam_policy_document.lambda_to_sns_policy.json
+  }
+
+}
+
+data "aws_iam_policy_document" "lambda_to_sns_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sns:Publish"]
+    resources = ["${aws_sns_topic.visit_couter.arn}"]
+  }
+}
+
+resource "aws_iam_role_policy" "dynamodb_read_log_policy" {
+  name   = "lambda-dynamodb-log-policy"
+  role   = aws_iam_role.lambda_assume_role.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+        "Action": [ "logs:*" ],
+        "Effect": "Allow",
+        "Resource": [ "arn:aws:logs:*:*:*" ]
+    },
+    {
+        "Action": [ "dynamodb:BatchGetItem",
+                    "dynamodb:GetItem",
+                    "dynamodb:GetRecords",
+                    "dynamodb:Scan",
+                    "dynamodb:Query",
+                    "dynamodb:GetShardIterator",
+                    "dynamodb:DescribeStream",
+                    "dynamodb:ListStreams" ],
+        "Effect": "Allow",
+        "Resource": [
+          "${aws_dynamodb_table.resume-dynamodb.arn}",
+          "${aws_dynamodb_table.resume-dynamodb.arn}/*"
+        ]
+    }
+
+  ]
+}
+EOF
 }
