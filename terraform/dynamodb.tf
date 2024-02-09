7d1decf28ba2263bfe3c7cea1772ca44198de69c
Chris <Chris@example2314.com>
2024-02-09 09:15:04 +0100

    added streams for dynamodb

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -2,6 +2,8 @@ resource "aws_dynamodb_table" "resume-dynamodb" {
   name         = "resume-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
+  stream_enabled = true
+  stream_view_type = "NEW_IMAGE"
 
   attribute {
     name = "id"
