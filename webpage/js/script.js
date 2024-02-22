303251d0d0f915b3f476a1e1ac46c1b229518e93
Chris <Chris@example2314.com>
2024-02-22 09:50:06 +0100

    Deleted old files

--- webpage/js/script.js
+++ webpage/js/script.js
@@ -1,5 +1,5 @@
 function getData() {
-    fetch("https://g9lls1fws2.execute-api.eu-west-1.amazonaws.com/prod/dev")
+    fetch("https://t0n28pnau8.execute-api.eu-west-1.amazonaws.com/prod/dev")
     .then(res => res.json())
     .then(data => document.getElementById("show").innerHTML = data.message)
     .catch(error => console.log("Error: ", error))
